0ae2b66912e16b920f14e53eba3b90884b5c8ca3
Nick Bynum <exander@live.com>
2020-04-03 19:14:01 -0600

    linked main to all controllers

--- app/main.js
+++ app/main.js
@@ -1,11 +1,15 @@
 import WeatherController from "./controllers/weather-controller.js";
 import TodoController from "./controllers/todo-controller.js";
+import quoteController from "./controllers/quote-controller.js";
+import imageController from "./controllers/image-controller.js";
 
-//TODO Dont forget to register all your controllers
+//TODO Dont forget to register all your controllers **done**
 class App {
   constructor() {
     this.weatherController = new WeatherController();
     this.todoController = new TodoController();
+    this.quoteController = new quoteController();
+    this.imageController = new imageController();
   }
 }
 
