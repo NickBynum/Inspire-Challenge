5094bd36311d5af902fd21d8748fdd61042f26c7
Nick Bynum <54619366+NickBynum@users.noreply.github.com>
2022-01-27 22:11:16 -0700

    updated quotes

--- app/models/quote.js
+++ app/models/quote.js
@@ -1,7 +1,7 @@
 export default class Quote {
   constructor(data) {
     this.id = data.id || ''
-    this.quote = data.body || ''
-    this.author = data.author || ''
+    this.quote = data.content || '“The greatest glory in living lies not in never falling, but in rising every time we fall.”'
+    this.author = data.author || 'Nelson Mandela'
   }
 }
